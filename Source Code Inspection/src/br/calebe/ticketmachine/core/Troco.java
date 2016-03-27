b1b5f9d7887f51dbd6a8be6a22ce9a2d18938788
Vitória <Vitria@NTDell002>
2016-03-26 23:56:21 -0300

    Issue #3 resolvido

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -2,6 +2,7 @@ package br.calebe.ticketmachine.core;
 
 import java.util.Iterator;
 
+
 /**
  *
  * @author Calebe de Paula Bianchini
@@ -47,40 +48,5 @@ class Troco {
     public Iterator<PapelMoeda> getIterator() {
         return new TrocoIterator(this);
     }
-
-    class TrocoIterator implements Iterator<PapelMoeda> {
-
-        protected Troco troco;
-
-        public TrocoIterator(Troco troco) {
-            this.troco = troco;
-        }
-
-        @Override
-        public boolean hasNext() {
-            for (int i = 6; i >= 0; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public PapelMoeda next() {
-            PapelMoeda ret = null;
-            for (int i = 6; i >= 0 && ret != null; i++) {
-                if (troco.papeisMoeda[i] != null) {
-                    ret = troco.papeisMoeda[i];
-                    troco.papeisMoeda[i] = null;
-                }
-            }
-            return ret;
-        }
-
-        @Override
-        public void remove() {
-            next();
-        }
-    }
+    
 }
