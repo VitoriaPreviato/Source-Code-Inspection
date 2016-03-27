524a38d20a064224ae81db95df828f98583f79e3
Lucas Pedro <Lucas Pedro@LP_Computer>
2016-03-27 04:16:24 -0300

    Issue #8 resolvido

--- Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/PapelMoeda.java
@@ -14,6 +14,14 @@ public class PapelMoeda {
         this.quantidade = quantidade;
     }
 
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
     public int getValor() {
         return valor;
     }
