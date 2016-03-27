2b034431db8193d58f6428c5a1d16ca05f2c9ce6
Lucas Pedro <Lucas Pedro@LP_Computer>
2016-03-27 03:23:38 -0300

    Issue #10 resolvido

--- Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/core/Troco.java
@@ -12,42 +12,32 @@ class Troco {
 
     public Troco(int valor) {
         papeisMoeda = new PapelMoeda[6];
-        int count = 0;
-        while (valor % 100 != 0) {
-            valor %= 100;
-            count++;
-        }
-        papeisMoeda[5] = new PapelMoeda(100, count);
-        count = 0;
-        while (valor % 50 != 0) {
-            valor %= 50;
-            count++;
-        }
-        papeisMoeda[4] = new PapelMoeda(50, count);
-        count = 0;
-        while (valor % 20 != 0) {
-            valor %= 20;
-            count++;
-        }
-        papeisMoeda[3] = new PapelMoeda(20, count);
-        count = 0;
-        while (valor % 10 != 0) {
-            valor %= 10;
-            count++;
-        }
-        papeisMoeda[2] = new PapelMoeda(10, count);
-        count = 0;
-        while (valor % 5 != 0) {
+        
+        int quant_notas = valor / 100;
+        valor %= 100;
+        papeisMoeda[5] = new PapelMoeda(100, quant_notas);
+        
+        quant_notas = valor / 50;
+        valor %= 50;        
+        papeisMoeda[4] = new PapelMoeda(50, quant_notas);
+        
+        quant_notas = valor / 20;
+        valor %= 20;        
+        papeisMoeda[3] = new PapelMoeda(20, quant_notas);
+        
+        quant_notas = valor / 10;
+        valor %= 10;       
+        papeisMoeda[2] = new PapelMoeda(10, quant_notas);
+        
+        if (valor != 6 && valor != 8) {
+            quant_notas = valor / 5;
             valor %= 5;
-            count++;
+            papeisMoeda[1] = new PapelMoeda(5, quant_notas);
         }
-        papeisMoeda[1] = new PapelMoeda(5, count);
-        count = 0;
-        while (valor % 2 != 0) {
-            valor %= 2;
-            count++;
-        }
-        papeisMoeda[0] = new PapelMoeda(2, count);
+        
+        quant_notas = valor / 2;
+        valor %= 2;
+        papeisMoeda[0] = new PapelMoeda(2, quant_notas);
     }
 
     public Iterator<PapelMoeda> getIterator() {
